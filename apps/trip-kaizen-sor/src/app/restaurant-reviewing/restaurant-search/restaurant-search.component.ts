ab7d5b1c3ca5edd6f3c7d20af4fff857afc99cc2
sdedieu <dedieu.sylvain38@gmail.com>
2022-05-19 15:16:59 +0200

    rm restaurant-search.service for tp

--- apps/trip-kaizen-sor/src/app/restaurant-reviewing/restaurant-search/restaurant-search.component.ts
+++ apps/trip-kaizen-sor/src/app/restaurant-reviewing/restaurant-search/restaurant-search.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BehaviorSubject, catchError, debounceTime, Observable, of, switchMap, tap } from 'rxjs';
 import { Restaurant } from '../models/restaurant';
-import { RestaurantSearchService } from '../services/restaurant-search.service';
 
 @Component({
   selector: 'tks-restaurant-search',
@@ -15,15 +14,13 @@ export class RestaurantSearchComponent implements OnInit {
   isLoading$ = new BehaviorSubject<boolean>(false);
   search$ = new BehaviorSubject<string>('');
 
-  constructor(private restaurantSearchService: RestaurantSearchService) {}
+  constructor() {}
 
   ngOnInit(): void {
     this.restaurants$ = this.search$.pipe(
       tap(() => this.isLoading$.next(true)),
       debounceTime(500),
-      switchMap(search => this.restaurantSearchService.find(search, this.urgent).pipe(
-        catchError(() => of([]))
-      )),
+      switchMap(search => of([])),
       tap(() => this.isLoading$.next(false))
     );
   }
