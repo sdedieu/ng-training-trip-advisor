a278e3869895ec2533154bf107ea18dc927d23db
Sylvain DEDIEU <sylvain.dedieu@kaizen-solutions.net>
2022-10-25 10:00:58 +0200

    fix jest runner

--- libs/hotel-search-lib/jest.config.ts
+++ libs/hotel-search-lib/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'hotel-lib',
+  displayName: 'hotel-search-lib',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -9,7 +9,7 @@ export default {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/libs/hotel-lib',
+  coverageDirectory: '../../coverage/libs/hotel-search-lib',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
